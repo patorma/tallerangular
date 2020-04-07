5ecd45c24f46df4108d2fc9d4c62cde5e6318479
patorma <pocontrera@ing.ucsc.cl>
2020-04-06 22:41:22 -0400

    comandos asociados a ng g c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about.component';
+import { MainpageComponent } from './pages/mainpage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
